92c3797fd6e8a0a02658757ffe661f3aba44371d
Chris Cruz <christopherc1331@gmail.com>
2019-08-13 20:26:14 -0700

    Added form semantics and change handler

--- team-builder/src/Components/Form.js
+++ team-builder/src/Components/Form.js
@@ -1,7 +1,67 @@
 import React, { useState } from "react";
 
 const Form = props => {
-  return <div>test</div>;
+  const [member, setMember] = useState({
+    name: "",
+    email: "",
+    role: "",
+    favColor: "",
+    quote: ""
+  });
+
+  const changeHandler = event => {
+    setMember({ ...member, [event.target.name]: event.target.value });
+  };
+  return (
+    <div className="formContainer">
+      <form>
+        <label htmlFor="name">Member Name</label>
+        <input
+          type="text"
+          name="name"
+          placeHolder="Type Name Here"
+          value={member.name}
+          onChange={changeHandler}
+        />
+
+        <label htmlFor="email">Member Email</label>
+        <input
+          type="text"
+          name="email"
+          placeHolder="Type Email Here"
+          value={member.email}
+          onChange={changeHandler}
+        />
+
+        <label htmlFor="role">Member Role</label>
+        <input
+          type="text"
+          name="role"
+          placeHolder="Type Role Here"
+          value={member.role}
+          onChange={changeHandler}
+        />
+
+        <label htmlFor="favColor">Favorite Color</label>
+        <input
+          type="text"
+          name="favColor"
+          placeHolder="Type Favourite Color Here"
+          value={member.favColor}
+          onChange={changeHandler}
+        />
+
+        <label htmlFor="quote">Quote</label>
+        <input
+          type="text"
+          name="quote"
+          placeHolder="Type Quote Here"
+          value={member.quote}
+          onChange={changeHandler}
+        />
+      </form>
+    </div>
+  );
 };
 
 export default Form;
--- team-builder/src/Components/TeamList.js
+++ team-builder/src/Components/TeamList.js
@@ -9,6 +9,8 @@ const TeamList = props => {
             <h2>{member.name}</h2>
             <em>{member.email}</em>
             <strong>{member.role}</strong>
+            <p>{member.favColor}</p>
+            <p>{member.quote}</p>
           </div>
         );
       })}
